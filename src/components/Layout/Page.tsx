0f5187fbd4b00d497f72a710aa08660b7dab6f43
Sai Charan <sripadas@iitbhilai.ac.in>
2025-03-28 09:32:21 +0530

    resolved issues2

--- src/components/Layout/Page.tsx
+++ src/components/Layout/Page.tsx
@@ -2,9 +2,10 @@ import {NextPage} from 'next';
 import Head from 'next/head';
 import {useRouter} from 'next/router';
 import {memo, PropsWithChildren} from 'react';
-import {HomepageMeta} from '../../data/dataDef';
 
 import Chatbot from '../../components/chatbot';
+import {HomepageMeta} from '../../data/dataDef';
+
 const Page: NextPage<PropsWithChildren<HomepageMeta>> = memo(({children, title, description}) => {
   const {asPath: pathname} = useRouter();
 
